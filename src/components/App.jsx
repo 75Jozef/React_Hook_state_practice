b30487f2218ef5a25607851297523f46905d6a56
75Jozef <64250704+75Jozef@users.noreply.github.com>
2020-06-25 20:15:04 +0200

    auto_clock_hooks_useState

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [state, change] = useState();
+
+  function seeTime() {
+    let time = new Date().toLocaleTimeString().slice(0, 8);
+    change(time);
+  }
+
+  setInterval(seeTime, 1000);
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{state}</h1>
+      <button onClick={seeTime}>Get Time</button>
     </div>
   );
 }
